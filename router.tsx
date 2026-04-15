00c05dab97ceee92fdc70a61e500a295484d9585
rousean <rousean@163.com>
2026-04-15 22:56:12 +0800

    commit

--- router.tsx
+++ router.tsx
@@ -1,4 +1,2 @@
 import { createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
-import { DefaultCatchBoundary } from './components/DefaultCatchBoundary'
-import { NotFound } from './components/NotFound'
